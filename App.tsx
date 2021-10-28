776f251367d1ee44f5c983377586e1b9cf7f6415
rodrigodiasnoronha <rodrigodnoronha@gmail.com>
2021-10-28 09:20:44 -0300

    aula 01

--- App.tsx
+++ App.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import SignIn from "./src/screens/SignIn";
 
+import {
+  Rajdhani_500Medium,
+  Rajdhani_400Regular,
+  Rajdhani_600SemiBold,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Rajdhani_500Medium,
+    Rajdhani_400Regular,
+    Rajdhani_600SemiBold,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <SignIn />
+    </>
+  );
 }
